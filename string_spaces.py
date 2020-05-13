012aa1e725c64f4a236cd82b72341acb4e6d27db
kempdave <dkemp@ryerson.ca>
2020-05-13 13:45:58 -0400

    Second github commit test

---
+++ string_spaces.py
@@ -0,0 +1,12 @@
+a = 'hello'
+b = 'world'
+
+c = a+b
+
+# no spaces added between strings
+print (a+b)
+print (c)
+print("hello" + "world")
+
+#adds spaces between strings
+print (a,b)
--- text_weave.py
+++ text_weave.py
@@ -4,6 +4,40 @@ import colour
 import PIL
 
 
+
+class pixel_square(object):
+    tag = 1   #used to store a tag for every instance in the class
+
+    def __init__(self, letter, width, height, colour):
+        self.letter = letter
+        self.width = width
+        self.height = height
+        self.colour = colour
+
+        #increments tag for the next instance created so each instance has a unique tag
+        self.id = pixel_square.tag
+        pixel_square.tag +=1
+
+    def __str__(self):
+        #return "letter:" + str(self.letter) + "width:" + str(self.wdith) + "height" + str(self.height) + "colour" + str(self.colour)
+        return "letter:"
+
+    def set_letter(self, new_letter=""):
+        self.letter = new_letter
+
+    def set_width(self, new_width=""):
+        self.letter = new_width
+
+    def set_height(self, new_height=""):
+        self.letter = new_height
+
+    def set_colour(self, new_colour=""):
+        self.letter = new_colour
+
+
+
+
+
 def hello_world():
     print('hello world')
 
@@ -14,6 +48,7 @@ def default():
 
 def main():
     print('hello world')
+    print(pixel_square)
 
 
 if __name__ == '__main__':
