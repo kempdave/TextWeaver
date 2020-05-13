02b75f5d230043f198bb5f9793da55fda687f48e
kempdave <dkemp@ryerson.ca>
2020-05-13 16:09:20 -0400

    Open text file and convert to list of char working

--- text_weave.py
+++ text_weave.py
@@ -3,15 +3,16 @@ import pygame
 import colour
 import PIL
 
-#Global variables
-filename = "filename.txt"
+# Global parameters
+filename = "texts/raven.txt"
 pixel_width = 10
 pixel_height = 20
 
 
-
 class pixel_square(object):
-    tag = 1   #used to store a tag for every instance in the class
+    """ This is a class for each "pixel" element in the final image."""
+
+    tag = 1  # used to store a tag for every instance in the class
 
     def __init__(self, letter, width, height, colour):
         self.letter = letter
@@ -19,12 +20,13 @@ class pixel_square(object):
         self.height = height
         self.colour = colour
 
-        #increments tag for the next instance created so each instance has a unique tag
+        # increments tag for the next instance created so each instance has a unique tag
         self.id = pixel_square.tag
-        pixel_square.tag +=1
+        pixel_square.tag += 1
 
     def __str__(self):
-        return "letter:" + str(self.letter) + "width:" + str(self.wdith) + "height" + str(self.height) + "colour" + str(self.colour)
+        return "letter: " + str(self.letter) + " width: " + str(self.width) + " height: " + str(
+            self.height) + " colour: " + str(self.colour)
 
     def set_letter(self, new_letter=""):
         self.letter = new_letter
@@ -39,23 +41,46 @@ class pixel_square(object):
         self.letter = new_colour
 
 
-
-
-
 def hello_world():
-    print('hello world')
+    print("hello world")
 
 
-def default():
-    print('running normally')
+def text_to_chars(filename):
+    '''
+    Reads in a text file and return a list with each charater as a separate item
+
+    Parameters:
+    filename (string)
+
+    Returns:
+    list: where each element is a char from the original text file
+    '''
+
+    with open(filename, "r", encoding='utf8') as f:
+        text = f.read()
+    text = text.lower()
+
+    char_list = ""
+
+    for char in text:
+        if char in "abcdefghijklmnopqrstuvwxyz 1234567890":
+            char_list = char_list + char
+
+    return char_list
 
 
 def main():
-    print('hello world')
+    print("hello world")
 
+    '''
     #test for __str__
     ps1 = pixel_square("a", 10, 10, 10)
     print(ps1)
+    '''
+
+    chars = text_to_chars(filename)
+    for i in range(len(chars)):
+        print(chars[i])
 
 
 if __name__ == '__main__':
